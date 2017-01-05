5ce99625903475a52ad902a63f3203cba77885ef
ninjatop <chenxiaoxu123@gmail.com>
2017-01-05 15:22:25 +0800

    TEST

---
+++ src/SpellTool.java
@@ -0,0 +1,24 @@
+/**
+* @author CHEN
+* @create_time Nov 15, 2016 12:28:10 PM
+*/
+
+public class SpellTool {
+
+	
+
+	
+
+	
+
+	
+	
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 0;
+		b += a +1;
+		System.out.println(b);
+		
+	}
+}
+
